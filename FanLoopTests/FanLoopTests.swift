0e1f26fb3bf602c33ba367d6742d36d93c7b22ea
Boray Chen <r0975929562@gmail.com>
2025-05-10 14:58:18 +0800

    Initial Commit

---
+++ FanLoopTests/FanLoopTests.swift
@@ -0,0 +1,17 @@
+//
+//  FanLoopTests.swift
+//  FanLoopTests
+//
+//  Created by Boray Chen on 2025/5/10.
+//
+
+import Testing
+@testable import FanLoop
+
+struct FanLoopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
