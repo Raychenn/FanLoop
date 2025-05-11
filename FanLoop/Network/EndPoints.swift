dec23464ac13471bac1a9804c0f67be52f627c0d
Boray Chen <r0975929562@gmail.com>
2025-05-11 17:39:47 +0800

    add more local video assets

--- FanLoop/Network/EndPoints.swift
+++ FanLoop/Network/EndPoints.swift
@@ -9,20 +9,27 @@ import Foundation
 
 struct VideoEndpoint {
     static let all: [Video] = [
-        Video(id: "100801", title: "Mature Couple Sharing a Lovely Moment", subTitle: "Mature Couple Sharing a Lovely Moment",
+        Video(id: "1", title: "A vertical video of a mature couple sharing a lovely moment at the park bench on a sunny day", subTitle: "A vertical video captures a mature couple sitting on a sunlit park bench, sharing a warm, affectionate moment. Surrounded by lush greenery, they smile and chat, radiating joy and connection on a bright, sunny day.",
               url: "video1"),
-        
-        Video(id: "100765", title: "Happy Couple with Green Background", subTitle: "Happy Couple with Green Background",
-              url: "video2"),
-        
-        Video(id: "1259", title: "Portrait of a Woman in a Pool", subTitle: "Portrait of a Woman in a Pool",
+        Video(id: "2", title: "Happy Couple with Green Background", subTitle: "A young couple stands closely together, smiling warmly against a green background. The man, with a man bun, embraces the woman from behind, exuding a sense of happiness and connection between them.", url: "video2"),
+        Video(id: "3", title: "Portrait of a Woman in a Pool", subTitle: "A woman with black hair wearing sunglasses in a blue outdoor pool. A girl getting a tan in summer.",
               url: "video3"),
-        
-        Video(id: "1238", title: "Man Holding Neon Light", subTitle: "Man Holding Neon Light",
+        Video(id: "4", title: "Woman's feet splashing in the pool", subTitle: "Vertical POV shot in slow motion of a woman's feet in a blue outdoor pool. Above view of a woman's legs in the water.",
               url: "video4"),
-        
-        Video(id: "100696", title: "Stylish Person in Arcade Setting", subTitle: "Stylish Person in Arcade Setting with Bright Colors",
-              url: "video5")
+        Video(id: "5", title: "Mother with her little daughter eating a marshmallow in nature", subTitle: "Young mother with her little daughter in her arms sitting around a campfire in nature, while eating roasted marshmallows together.",
+              url: "video5"),
+        Video(id: "6", title: "Employee Receiving Bad News on Office Call", subTitle: "A corporate employee taking a phone call in an office, with their face replaced by a sad cry emoji, as if receiving bad news. Perfect for showcasing workplace dynamics or real-life moments in a lighthearted way.",
+              url: "video6"),
+        Video(id: "7", title: "Man holding neon light", subTitle: "Bald man with a short beard wearing a large jean jacket holds a long tubular neon light thatch",
+              url: "video7"),
+        Video(id: "8", title: "Stylish person in arcade setting with bright colors", subTitle: "A person with stylish long braids and red high-heeled boots leans casually against a bright yellow motorcycle, surrounded by vivid red and blue lights, creating a dynamic and edgy atmosphere.",
+              url: "video8"),
+        Video(id: "9", title: "Two avenues with many cars traveling at night", subTitle: "Two avenues with opposite directions, with many cars passing, at night, seen in a shot that runs from left to right.",
+              url: "video9"),
+        Video(id: "10", title: "Young mother with her little daughter decorating a Christmas tree", subTitle: "Young mother with her little daughter, preparing a golden ribbon bow to decorate a Christmas tree.",
+              url: "video10"),
+        Video(id: "11", title: "Skilled programmer writing codes on a computer", subTitle: "Skilled programmer typing code quickly, on a computer, in a close-up shot behind him, which focuses on his face and the computer screen.",
+              url: "video11"),
     ]
 }
 
